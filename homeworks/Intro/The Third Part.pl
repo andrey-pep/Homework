d652e37c03cf336cf3de4a6362353f2251ec082c
andrey-pep <andrey-pep@mail.ru>
2016-10-18 14:50:57 +0400

    Update The Third Part.pl

--- homeworks/Intro/The Third Part.pl
+++ homeworks/Intro/The Third Part.pl
@@ -5,16 +5,11 @@ use warnings;
 
 my @massofmass=();
 my $count=0;
-my $input = 'text.txt';
-open(my $fl, '<', $input)
-        or die "Counld not open file '$input' $!";
       
-while (<$fl>) {
+while (<>) {
 my @timemas=();
 @timemas=split(/;|\n/, $_);
 @massofmass[$count] = \@timemas;
 $count++;
 }
 print Dumper(@massofmass);
-close($fl)
-  or die "Counld not close file '$input' $!";
