df42f2f40cf9130320bb2d25d92779ad1ed8205c
andrey-pep <andrey-pep@mail.ru>
2016-11-23 21:10:29 +0300

    Add project

---
+++ homeworks/music_library/lib/Local/MaxLen.pm
@@ -0,0 +1,12 @@
+package Local::MaxLen;
+
+use strict;
+use warnings;
+use diagnostics;
+use base qw(Exporter);
+our @EXPORT_OK = qw( max_len );
+our @EXPORT = qw( max_len );
+
+sub max_len {
+    
+}
--- homeworks/music_library/lib/Local/MusicLibrary.pm
+++ homeworks/music_library/lib/Local/MusicLibrary.pm
@@ -3,6 +3,7 @@ package Local::MusicLibrary;
 use strict;
 use warnings;
 use diagnostics;
+use Local::MaxLen;
 use base qw(Exporter);
 our @EXPORT_OK = qw( table_out );
 our @EXPORT = qw( table_out );
@@ -28,10 +29,16 @@ our $VERSION = '1.00';
 
 sub table_out {
     my @coloms;
+    my %len_hash;
     my $i = 0;
-    while ( ref ( $coloms[$i] = shift ) ne "HASH" ) { $i++; }
+    while ( ref ( $coloms[$i] = shift ) ne "HASH" ) { $i++; }       #считываем данные о столбцах
     unshift(@_,pop (@coloms));
-    my @music = @_;
+    my @music = @_;                                                 #и считываем массив с треками
+    
+    for my $item (@coloms) {
+        $len_hash{$item} = max_len(@music);
+    }
+    
     p @coloms;
     return @music if wantarray;
     1;
