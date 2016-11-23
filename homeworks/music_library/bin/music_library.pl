24b5600fec46006775e575c9ec78695328f70c50
andrey-pep <andrey-pep@mail.ru>
2016-11-24 00:21:50 +0300

    Add project

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -8,6 +8,7 @@ use lib "$FindBin::Bin/../lib";
 use Local::Parse;
 use Local::MusicLibrary;
 use DDP;
+use feature 'switch';
 
 our $VERSION = '1.00';
 
@@ -16,9 +17,16 @@ my $i = 0;
 my @coloms = ("band","year","album","treck","form");
 
 while (<>) {
-    $music[$i++] = add_treck($_);
+    if (/\.\/.+\/\d{4} - .+\/.+\.[\w]+\s?/) {
+        $music[$i++] = add_treck($_)
+    }
 }
 
+for my $item (@ARGV) {
+    given ($item) {
+        when("--band") {say "lol";}
+    }
+}
 my @data = table_out(@coloms,@music);
 #p @music;
 #p @data;
