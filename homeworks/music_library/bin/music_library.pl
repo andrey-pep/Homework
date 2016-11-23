b8c99e30c8dc88fb2136ce5ecc0b904199442f2b
andrey-pep <andrey-pep@mail.ru>
2016-11-23 23:50:47 +0300

    Add project

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -13,12 +13,13 @@ our $VERSION = '1.00';
 
 my @music;
 my $i = 0;
+my @coloms = ("band","year","album","treck","form");
 
 while (<>) {
     $music[$i++] = add_treck($_);
 }
 
-my @data = table_out("band","year","album","treck","form",@music);
+my @data = table_out(@coloms,@music);
 #p @music;
 #p @data;
 #say $music[19]{"band"};
