46e16b2e396dceae5d3705488fa108e187bc7466
andrey-pep <andrey-pep@mail.ru>
2016-11-30 21:07:12 +0300

    Add project

--- homeworks/music_library/blib/lib/Local/MusicLibrary.pm
+++ homeworks/music_library/blib/lib/Local/MusicLibrary.pm
@@ -29,41 +29,40 @@ our $VERSION = '1.2';
 =cut
 
 sub table_out {
-    my @columns;
     my %len_hash;
     my $i = 0;
     my $string_len = 0;
     my $columns = shift;
     #while ( ref ( $columns[$i] = shift ) ne "HASH" ) { $i++; }       #считываем данные о столбцах
     #unshift(@_,pop (@columns));
-    if (@columns == 0) {
+    if (@$columns == 0) {
         exit;
     }
     my $music = shift;                                                 #и считываем массив с треками
     my $separator = '|';
-    for my $item (@columns) {
+    for my $item (@$columns) {
         if (defined $item) {
-            $len_hash{$item} = max_len($item,@music);
+            $len_hash{$item} = max_len($item,@$music);
             $string_len+= $len_hash{$item} + 2;
             $separator = $separator.sprintf("%s+", '-'x($len_hash{$item} + 2));
         }
     }
     chop $separator;
     $separator = $separator.'|';
-    $string_len += @columns + 1;
+    $string_len += @$columns + 1;
     printf ("/%s\\\n",'-'x($string_len - 2));
     $i = 0;
-    for (@music) {       
-        for my $item (@columns) {
-            printf ("| %$len_hash{$item}s ", $music[$i]{$item});
+    for (@$music) {       
+        for my $item (@$columns) {
+            printf ("| %$len_hash{$item}s ", $$music[$i]{$item});
         }
         print "|\n";
-        last if $i == @music - 1;
+        last if $i == @$music - 1;
         say $separator;
         $i++;
     }
     printf ("\\%s/\n",'-'x($string_len - 2));
-    return @music if wantarray;
+    return $music if wantarray;
     1;
 }
 
