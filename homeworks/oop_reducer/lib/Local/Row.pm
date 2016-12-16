4d8e001a9b64df6b60bd0c645dde60ef63c5bf9d
andrey-pep <andrey-pep@mail.ru>
2016-12-16 14:40:29 +0300

    Made Row. Started Reducer and Source

--- homeworks/oop_reducer/lib/Local/Row.pm
+++ homeworks/oop_reducer/lib/Local/Row.pm
@@ -1,8 +1,17 @@
 package Local::Row; {
 use strict;
 use warnings;
+use utf8;
 
 sub new {
-    my ( $class, %params) = @_;
+    my ($class, %params) = @_;
+    my $str = $class -> parse($params{$str});
+    bless $str,\$class;
+}
+
+sub get {
+    my ($self, $name, $default) = @_;
+    if (defined $self -> {$name}) { return $self -> {name}; }
+    else { return $default; }
 }
 }
--- homeworks/oop_reducer/lib/Local/Source.pm
+++ homeworks/oop_reducer/lib/Local/Source.pm
@@ -5,13 +5,5 @@ use utf8;
 
 sub new {
     my ($class, %params) = @_;
-    my $str = $class -> parse($params{$str});
-    bless $str,\$class;
-}
-
-sub get {
-    my ($self, $name, $default) = @_;
-    if (defined $self -> {$name}) { return $self -> {name}; }
-    else { return $default; }
-}
+    
 }
